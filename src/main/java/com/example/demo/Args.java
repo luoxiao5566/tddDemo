67adb8cf09bdbbd249bf9bd7f8ba7f68cafc23bf
LuoXiao <xiao.luo@thoughtworks.com>
2022-05-06 22:08:22 +0800

    [TECH] feat: add multi options logic

--- src/main/java/com/example/demo/Args.java
+++ src/main/java/com/example/demo/Args.java
@@ -8,14 +8,33 @@ import java.util.List;
 public class Args {
     public static <T> T parse(Class<T> optionsClass, String... args) {
         try {
-            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-            Parameter parameter = constructor.getParameters()[0];
-            Option option = parameter.getAnnotation(Option.class);
             List<String> argument = Arrays.asList(args);
-            return (T) constructor.newInstance(argument.contains("-" + option.value()));
+            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+
+            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(argument, it)).toArray();
+
+            return (T) constructor.newInstance(values);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    private static Object parseOption(List<String> argument, Parameter parameter) {
+        Object value = null;
+        Option option = parameter.getAnnotation(Option.class);
+
+        if (parameter.getType() == boolean.class) {
+            value = argument.contains("-" + option.value());
+        }
+        if (parameter.getType() == int.class) {
+            int index = argument.indexOf("-" + option.value());
+            value = Integer.parseInt(argument.get(index + 1));
+        }
+        if (parameter.getType() == String.class) {
+            int index = argument.indexOf("-" + option.value());
+            value = argument.get(index + 1);
+        }
+        return value;
     }
 }
--- src/test/java/com/example/demo/ArgsTest.java
+++ src/test/java/com/example/demo/ArgsTest.java
@@ -29,26 +29,46 @@ public class ArgsTest {
     static record BooleanOption(@Option("l") boolean logging) {
     }
 
-    // todo:     -integer -p 8080
-
-    // todo:     -String -d /usr/logs
-    // todo: multi optionsL: -l -p 8080 -d /usr/logs
-    // sad path:
-    //  -boll -l t / -l t f
-    //  -int -p/ -p 8080 8081
-    //  -string -d/ -d /usr/logs /usr/vars
-    // default value
-    //  -boll:false
-    //  -int:0
-    //  -string:""
     @Test
-    @Disabled
-    public void should_example_1() {
-        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
+    public void should_parse_int_as_option_value() {
+        IntOption option = Args.parse(IntOption.class, "-p", "8080");
+        assertEquals(8080, option.port());
+
+    }
+
+    static record IntOption(@Option("p") int port) {
+    }
+
+    @Test
+    public void should_parse_string_as_option_value() {
+        StringOption option = Args.parse(StringOption.class, "-d", "/usr/logs");
+        assertEquals("/usr/logs", option.directory());
+
+    }
+
+    static record StringOption(@Option("d") String directory) {
+    }
+
+    // todo: multi optionsL: -l -p 8080 -d /usr/logs
+    @Test
+    public void should_parse_multi_options() {
+        MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
         assertTrue(options.logging());
         assertEquals(8080, options.port());
         assertEquals("/usr/logs", options.directory());
     }
+
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
+    // sad path:
+    // todo: -boll -l t / -l t f
+    // todo: -int -p/ -p 8080 8081
+    // todo: -string -d/ -d /usr/logs /usr/vars
+    // default value
+    // todo: -boll:false
+    // todo: -int:0
+    // todo: -string:""
+
 
     //-g this is a list -d 1 2 -3 5
     @Test
@@ -57,9 +77,6 @@ public class ArgsTest {
         ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
         assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
         assertArrayEquals(new int[]{1, 2, -3, 5}, options.decimals());
-    }
-
-    static record Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
     }
 
     static record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
